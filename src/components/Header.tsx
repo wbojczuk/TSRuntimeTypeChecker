b71ee72a5a1e5b260467a46fa5bd87333767b799
William Bojczuk <williambojczuk@gmail.com>
2023-08-01 14:35:52 -0700

    fixed icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header>
         <h1 id="siteTitle">Runtime Type Checker</h1>
         <a id="githubIcon" href="https://github.com/wbojczuk/TSRuntimeTypeChecker/tree/main" target='_blank'>
-            <img src="/img/github_icon.svg" alt="github icon" title='Github Repository'/>
+            <img src="./img/github_icon.svg" alt="github icon" title='Github Repository'/>
         </a>
     </header>
   )
