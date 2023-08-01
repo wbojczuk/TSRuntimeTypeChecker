bd8bed464071dfade7a985c19e7c7ebf689576d1
William Bojczuk <williambojczuk@gmail.com>
2023-08-01 14:39:30 -0700

    $1

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,7 +5,7 @@ export default function Content() {
   return (
     <div id="content">
         <article id="siteDescription">
-        Created to make validating data types and content during runtime as easy as can be. With an unzipped size of only <span className="highlight-green">~3kb</span>, simply use the CDN, the file from GitHub, or the NPM package to use the function.
+        Created to make validating data types and content during runtime as easy as can be! With an unzipped size of only <span className="highlight-green">~3kb</span>, simply use the CDN, the file from GitHub, or the NPM package to use the function.
         </article>
         
         <ContentBoxes />
