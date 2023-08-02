165c124f4e5d13dab0e5ec08415ee1873e0d8473
William Bojczuk <williambojczuk@gmail.com>
2023-08-02 10:40:26 -0400

    Added some content

--- src/components/content/contentBoxData.ts
+++ src/components/content/contentBoxData.ts
@@ -6,17 +6,60 @@ export const contentBoxData: contentBoxDataType = [
         text: "It’s extremely easy to use. One call to the typeChecker function and two parameters will return a boolean respresenting if the data is the way it should be."
     },
     {
-        code: `// Primitive Types\r"string", "boolean", "number", "undefined"\r\r// Complex Types\r"object", "array", "tuple"\r\r// Additional Types\r "all"`,
-        text: "Many different data types are supported including undefined values, strings, booleans, numbers, arrays, objects, and tuples. Also the \"all\" type that matches any value."
+        code: 
+`// Data Type                 Representation
+
+ String:                         "string"
+
+ Number:                      "number"
+
+ Boolean:                      "boolean"
+
+ Undefined:                   "undefined"
+
+ Any_Value:                   "all"
+
+ Empty_Arrays:              [  ] or "array"
+
+ Non_Empty_Arrays:     [ "type" ]
+
+ Tuples:                         [ "type", "type" ]
+ 
+ Empty_Objects:           { } or "object"
+ 
+ Non_Empty_Objects:   {"prop": "type"}
+    `,
+    
+        text: "Many different data types are supported including undefined values, strings, booleans, numbers, arrays, objects, and tuples. Also the \"all\" type that matches any value. The visual display on the left shows how to represent different data types and templates."
     },
     {
         code: `const template = {
             "name": "string",
             "age": "number",
-            "friendNames": ["string"]
-        }
+            "friendData": [ 
+                {
+                    "name": "string",
+                    "age": "number"
+                }
+             ]
+        };
         
         typeChecker(value, template);`,
-        text: "Supports templates to check complex types against like arrays, objects, and tuples. Great for validating data received on the server-side"
+        text: "Supports templates to check objects against. Great for validating data received from the client or an API on the server-side. The script will check as deep as the object goes."
+    },
+    {
+        code: `// Optional Object Property
+
+const template = {
+    "prop?": "boolean"
+};
+            
+
+// Multiple Allowed Types
+
+const type = "string | number";
+
+typeChecker(value, type);`,
+        text: "You can also specify two or more primitive types that a value can be by seperating multiple values with \"|\". Or specify an object property that is optional by adding a \"?\" to the end of a property name."
     }
 ]
